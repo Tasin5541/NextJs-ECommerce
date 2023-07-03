d92e8e1e075365734fc8a75f8f3f7602036429ad
Tasin5541 <tasinmiftaulmannan@gmail.com>
2023-07-03 19:11:34 +0600

    Add: suppressHydrationWarning

--- src/nextjs.ecommerce.admin/app/layout.tsx
+++ src/nextjs.ecommerce.admin/app/layout.tsx
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 
-import { ModalProvider } from "@/providers/ModalProvider";
-
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ModalProvider } from "@/providers/ModalProvider";
 
 import "./globals.css";
 
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body suppressHydrationWarning={true} className={inter.className}>
           <ModalProvider />
           {children}
         </body>
