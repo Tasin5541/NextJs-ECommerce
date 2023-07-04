56953e4cd754878477a2b7be5bea658a59dd03a5
Tasin5541 <tasinmiftaulmannan@gmail.com>
2023-07-04 20:47:50 +0600

    Add: Navbar skeleton

--- src/nextjs.ecommerce.admin/app/(dashboard)/[storeId]/layout.tsx
+++ src/nextjs.ecommerce.admin/app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { auth } from "@clerk/nextjs";
 
+import Navbar from "@/components/Navbar";
 import prismadb from "@/lib/prismadb";
 
 export default async function DashboardLayout({
@@ -30,7 +31,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>Navigation</div>
+      <Navbar />
       {children}
     </>
   );
---
+++ src/nextjs.ecommerce.admin/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { redirect } from "next/navigation";
+
+import { auth, UserButton } from "@clerk/nextjs";
+
+import MainNav from "@/components/MainNav";
+import prismadb from "@/lib/prismadb";
+
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
