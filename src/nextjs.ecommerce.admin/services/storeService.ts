ee195e9c5753623c0f89e91022e310e645179731
Tasin5541 <tasinmiftaulmannan@gmail.com>
2023-07-13 21:00:55 +0600

    Feat [admin]: Store settings to allow update and delete

--- src/nextjs.ecommerce.admin/services/storeService.ts
+++ src/nextjs.ecommerce.admin/services/storeService.ts
@@ -4,3 +4,11 @@ import { StoreFormSchema, StoreSchema } from "@/types/store";
 export const createStore = (body: StoreFormSchema): Promise<StoreSchema> => {
   return requests.post(`/api/stores`, body);
 };
+
+export const updateStore = (storeId: string, body: StoreFormSchema): Promise<StoreSchema> => {
+  return requests.patch(`/api/stores/${storeId}`, body);
+};
+
+export const deleteStore = (storeId: string): Promise<StoreSchema> => {
+  return requests.delete(`/api/stores/${storeId}`);
+};
